373696851146a12ca38277dd20725586967fc5d6
Eric Zepeda <zepedaeric@gmail.com>
2020-10-08 12:20:25 -0700

    Use axios to send data from microservice

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serveStatic = require('serve-static');
 const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 const server = express();
-
+const axios = require('axios');
 server.use(morgan('dev'));
 server.use(serveStatic('./client/'));
 
@@ -18,9 +18,9 @@ server.get('/product/:itemId', (req, res) => {
   ) {
     res.status(404).send('itemID invalid');
   } else {
-    res.redirect(
-      `http://54.176.112.135:3002/descriptionObject/${itemIdNumber}`
-    );
+    axios
+      .get(`http://54.176.112.135:3002/descriptionObject/${itemIdNumber}`)
+      .then(({ data }) => res.send(data));
   }
 });
 
